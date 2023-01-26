2e1b7897105191720a2a7b8ecebdfd1adb49f0a6
Reggie Lee Murray <lee_murray@unitec.edu>
2023-01-26 13:57:29 -0600

    carga masiva empleados QoL update

--- src/app/carga-masiva/carga-masiva.component.ts
+++ src/app/carga-masiva/carga-masiva.component.ts
@@ -7,6 +7,7 @@ import { TipoplanillaService } from 'src/app/services/tipoplanilla.service';
 import { DepartamentosService } from 'src/app/services/departamentos.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzModalService } from 'ng-zorro-antd/modal';
+import { HttpClient } from '@angular/common/http';
 
 const EXCEL_TYPE =
   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
@@ -44,7 +45,8 @@ export class CargaMasivaComponent implements OnInit {
   isVisibleId = false;
   isVisibleRepetidos = false;
   isVisibleCedula = false;
-  DEBUG = false;
+  DEBUG = true;
+  httpClient: HttpClient;
 
   constructor(
     private fb: FormBuilder,
@@ -52,11 +54,13 @@ export class CargaMasivaComponent implements OnInit {
     private EmpleadoinactivoService: EmpleadoinactivoService,
     private TipoplanillaService: TipoplanillaService,
     private DepartamentosService: DepartamentosService,
-    private modal: NzModalService
+    private modal: NzModalService,
+    private http: HttpClient
   ) {
     this.reqForm = this.fb.group({
       archivo: ['', Validators.required],
     });
+    this.httpClient = http;
   }
   ngOnInit(): void {
     // ENABLE/DISABLE Console Logs
@@ -91,6 +95,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: red;">ADVERTENCIA: Contiene colaboradores ya existentes en el sistema.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOkDanger: true,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
@@ -105,6 +110,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: red;">ADVERTENCIA: Número de cédula inadmisible. Debe tener 13 números sin guiones y estar formato texto.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOkDanger: true,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
@@ -119,6 +125,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: red;">ADVERTENCIA: Existe un error de tipografía. Lea las instrucciones y revise si ingreso correctamente el genero o los ID requeridos.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOkDanger: true,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
@@ -130,9 +137,10 @@ export class CargaMasivaComponent implements OnInit {
       nzCentered: true,
       nzTitle: 'Archivo Inconsistente',
       nzContent:
-        '<b style="color: red;">ADVERTENCIA: Registros duplicados en el archivo. TIP: Nombre, número de cedula ó email duplicado.</b>',
+        '<b style="color: red;">ADVERTENCIA: Registros duplicados en el archivo. TIP: Nombre ó número de cedula duplicado.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOkDanger: true,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
@@ -146,6 +154,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: red;">ADVERTENCIA: No se actualizarán los colaboradores. TIP: Usa nuestra plantilla y sigue las instrucciones o asegúrate de elejir el archivo correcto. Revise si hay campos en blanco.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOkDanger: true,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
@@ -159,6 +168,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: yellow;">ADVERTENCIA: No se actualizarán los colaboradores. TIP: Asegúrate de elejir el archivo correcto.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
     });
@@ -171,6 +181,7 @@ export class CargaMasivaComponent implements OnInit {
         '<b style="color: yellow;">ADVERTENCIA: Solo se permite un archivo en formato Excel.</b>',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
     });
@@ -182,6 +193,7 @@ export class CargaMasivaComponent implements OnInit {
       nzContent: 'Listados actualizados',
       nzOkText: 'Okay',
       nzOkType: 'primary',
+      nzClosable: false,
       nzOnOk: () => this.clean(),
       nzOnCancel: () => this.clean(),
     });
@@ -194,15 +206,35 @@ export class CargaMasivaComponent implements OnInit {
     } else {
       const reader: FileReader = new FileReader();
       reader.onload = (e: any) => {
+        const headers = [
+          'nombres',
+          'apellidos',
+          'n_Cedula',
+          'direccion',
+          'fechaNac',
+          'fechaIngreso',
+          'genero',
+          'salarioBase',
+          'departamentoID',
+          'planillaID',
+        ];
         const bstr: string = e.target.result;
         const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
         var nombreHoja = wb.SheetNames; // regresa un array
-        let datos = XLSX.utils.sheet_to_json(wb.Sheets[nombreHoja[0]]);
+        // let datos = XLSX.utils.sheet_to_json(wb.Sheets[nombreHoja[0]]);
+        let datos = XLSX.utils.sheet_to_json(wb.Sheets[nombreHoja[0]], {
+          header: headers,
+        });
+        //remueve las instrucciones del archivo antes de leerlo, no debe incluir titulos
+        datos = datos.slice(8);
+        console.log('DATOS', datos);
         for (let i = 0; i < datos.length; i++) {
           const dato = datos[i];
           this.jDatos.push({
             ...(dato as object),
           });
+          this.jDatos[i].email = 'email@xyz.com';
+          console.log(this.jDatos, dato);
           if (
             this.jDatos[i].fechaNac != undefined ||
             this.jDatos[i].fechaIngreso != undefined
@@ -219,40 +251,40 @@ export class CargaMasivaComponent implements OnInit {
           }
           for (let k = 0; k < this.listInicialEmpleadoActivo.length; k++) {
             if (
-              (this.jDatos[i].n_Cedula ==
+              (this.jDatos[i].n_Cedula ===
                 this.listInicialEmpleadoActivo[k].n_Cedula ||
-                this.jDatos[i].email ==
-                  this.listInicialEmpleadoActivo[k].email ||
-                this.jDatos[i].nombres + ' ' + this.jDatos[i].apellidos ==
+                // this.jDatos[i].email ===
+                //   this.listInicialEmpleadoActivo[k].email ||
+                this.jDatos[i].nombres + ' ' + this.jDatos[i].apellidos ===
                   this.listInicialEmpleadoActivo[k].nombres +
                     ' ' +
                     this.listInicialEmpleadoActivo[k].apellidos) &&
-              this.isVisibleExistente == false
+              this.isVisibleExistente === false
             ) {
               this.existente();
             }
           }
           if (
             this.jDatos[i].n_Cedula.length < 13 &&
-            this.isVisibleCedula == false
+            this.isVisibleCedula === false
           ) {
             this.cedulaInconsistente();
           }
           if (
-            this.jDatos[i].genero == 'Masculino' ||
-            this.jDatos[i].genero == 'Femenino' ||
-            this.jDatos[i].genero == 'femenino' ||
-            this.jDatos[i].genero == 'masculino' ||
-            this.jDatos[i].genero == 'F' ||
-            this.jDatos[i].genero == 'M' ||
-            this.jDatos[i].genero == 'f' ||
-            this.jDatos[i].genero == 'm'
+            this.jDatos[i].genero === 'Masculino' ||
+            this.jDatos[i].genero === 'Femenino' ||
+            this.jDatos[i].genero === 'femenino' ||
+            this.jDatos[i].genero === 'masculino' ||
+            this.jDatos[i].genero === 'F' ||
+            this.jDatos[i].genero === 'M' ||
+            this.jDatos[i].genero === 'f' ||
+            this.jDatos[i].genero === 'm'
           ) {
             if (
-              this.jDatos[i].genero == 'Masculino' ||
-              this.jDatos[i].genero == 'masculino' ||
-              this.jDatos[i].genero == 'M' ||
-              this.jDatos[i].genero == 'm'
+              this.jDatos[i].genero === 'Masculino' ||
+              this.jDatos[i].genero === 'masculino' ||
+              this.jDatos[i].genero === 'M' ||
+              this.jDatos[i].genero === 'm'
             ) {
               this.jDatos[i].genero = 'Masculino';
             } else {
@@ -262,7 +294,7 @@ export class CargaMasivaComponent implements OnInit {
             this.malFormato();
           }
         }
-        if (this.jDatos.length == 0) {
+        if (this.jDatos.length === 0) {
           this.archivoVacio();
         } else {
           console.log(
@@ -276,7 +308,7 @@ export class CargaMasivaComponent implements OnInit {
             this.listEmpleadoActivo[i].nombres = this.jDatos[i].nombres;
             this.listEmpleadoActivo[i].apellidos = this.jDatos[i].apellidos;
             this.listEmpleadoActivo[i].direccion = this.jDatos[i].direccion;
-            this.listEmpleadoActivo[i].email = this.jDatos[i].email;
+            this.listEmpleadoActivo[i].email = 'email@xyz.com';
             this.listEmpleadoActivo[i].genero = this.jDatos[i].genero;
             this.listEmpleadoActivo[i].n_Cedula = this.jDatos[i].n_Cedula;
             this.listEmpleadoActivo[i].salarioBase = this.jDatos[i].salarioBase;
@@ -287,31 +319,31 @@ export class CargaMasivaComponent implements OnInit {
           for (let i = 0; i < this.jDatos.length; i++) {
             for (let k = 0; k < this.listDeptos.length; k++) {
               if (
-                this.jDatos[i].departamentoID == this.listDeptos[k].id &&
-                this.isVisibleId == false
+                this.jDatos[i].departamentoID === this.listDeptos[k].id &&
+                this.isVisibleId === false
               ) {
                 this.listEmpleadoActivo[i].departamentoID =
                   this.listDeptos[k].descripcion;
                 k = this.listDeptos.length - 1;
               } else if (
-                k == this.listDeptos.length - 1 &&
-                this.isVisibleId == false
+                k === this.listDeptos.length - 1 &&
+                this.isVisibleId === false
               ) {
                 this.malFormato();
               }
             }
             for (let j = 0; j < this.listTP.length; j++) {
               if (
-                this.jDatos[i].planillaID == this.listTP[j].id &&
-                this.isVisibleId == false
+                this.jDatos[i].planillaID === this.listTP[j].id &&
+                this.isVisibleId === false
               ) {
                 this.listEmpleadoActivo[i].planillaID =
                   this.listTP[j].descripcion;
                 this.listEmpleadoActivo[i].tipo = this.listTP[j].tipo;
                 j = this.listTP.length - 1;
               } else if (
-                j == this.listTP.length - 1 &&
-                this.isVisibleId == false
+                j === this.listTP.length - 1 &&
+                this.isVisibleId === false
               ) {
                 this.malFormato();
               }
@@ -324,11 +356,10 @@ export class CargaMasivaComponent implements OnInit {
             x++;
             if (
               x < this.listFinal.length &&
-              this.isVisibleRepetidos == false &&
-              (this.listFinal[i].nombres + ' ' + this.listFinal[i].apellidos ==
+              this.isVisibleRepetidos === false &&
+              (this.listFinal[i].nombres + ' ' + this.listFinal[i].apellidos ===
                 this.listFinal[x].nombres + ' ' + this.listFinal[x].apellidos ||
-                this.listFinal[i].n_Cedula == this.listFinal[x].n_Cedula ||
-                this.listFinal[i].email == this.listFinal[x].email)
+                this.listFinal[i].n_Cedula === this.listFinal[x].n_Cedula)
             ) {
               this.repetidos();
             }
@@ -339,27 +370,38 @@ export class CargaMasivaComponent implements OnInit {
     }
   }
   exportExcel(): void {
-    /* table id is passed over here */
-    let colaboradores = document.getElementById('template');
-    // let departamentos = document.getElementById('deptos');
-    // let planillas = document.getElementById('planillas');
-    /* worksheets */
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(colaboradores);
-    // const wsDeptos: XLSX.WorkSheet = XLSX.utils.table_to_sheet(departamentos);
-    // const wsPlanillas: XLSX.WorkSheet = XLSX.utils.table_to_sheet(planillas);
-    /* generate workbook and add the worksheet */
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Colaboradores');
-    // XLSX.utils.book_append_sheet(wb, wsDeptos, 'Departamentos');
-    // XLSX.utils.book_append_sheet(wb, wsPlanillas, 'Planillas');
-    /* save to excel-file which will be downloaded */
-    XLSX.writeFile(wb, 'Carga Masiva del ' + this.today + '.xlsx');
-    /* write and save file */
-    const excelBuffer: any = XLSX.write(wb, {
-      bookType: 'xlsx',
-      type: 'array',
-    });
-    const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
+    let filepath = './assets/cargaMasiva.xlsx';
+    this.httpClient
+      .get(filepath, { responseType: 'arraybuffer' })
+      .subscribe((res: any) => {
+        console.log(res);
+        const temporal: XLSX.WorkBook = XLSX.read(res, {
+          type: 'buffer',
+        });
+        var nombreHoja = temporal.SheetNames;
+        const wsColaboradores: XLSX.WorkSheet = temporal.Sheets[nombreHoja[0]];
+        /* table id is passed over here */
+        let departamentos = document.getElementById('deptos');
+        let planillas = document.getElementById('planillas');
+        /*html tables*/
+        const wsDeptos: XLSX.WorkSheet =
+          XLSX.utils.table_to_sheet(departamentos);
+        const wsPlanillas: XLSX.WorkSheet =
+          XLSX.utils.table_to_sheet(planillas);
+        /* generate workbook and add the worksheet */
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, wsColaboradores, 'Colaboradores');
+        XLSX.utils.book_append_sheet(wb, wsDeptos, 'Departamentos ID');
+        XLSX.utils.book_append_sheet(wb, wsPlanillas, 'Planillas ID');
+        /* save to excel-file which will be downloaded */
+        XLSX.writeFile(wb, 'Carga Masiva del ' + this.today + '.xlsx');
+        /* write and save file */
+        const excelBuffer: any = XLSX.write(wb, {
+          bookType: 'xlsx',
+          type: 'array',
+        });
+        const archivo: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
+      });
   }
   upload() {
     document.getElementById('btnSubir').setAttribute('disabled', 'disabled');
@@ -381,7 +423,7 @@ export class CargaMasivaComponent implements OnInit {
         planillaID: '',
         direccion: '',
         salarioBase: 0,
-        email: '',
+        email: 'email@xyz.com',
         fechaIngreso: new Date(),
         fechaNac: new Date(),
         genero: '',
