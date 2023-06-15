1ec8d7dbaf4a32f99f56b1f0bf508d60b27069a6
Reggie Lee Murray <lee_murray@unitec.edu>
2023-06-15 15:48:37 -0600

    comprobantes / incompleto update

--- src/app/components/nomina/nomina.component.ts
+++ src/app/components/nomina/nomina.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc0f1a361f9e44bcdced22b9ae0d12af5a6ed56f4382ab8fccd98c1ef1ed35bb
-size 227839
+oid sha256:13f48c1c3f10252d16ec81bf6929a735373303ddf5d083e72f8019544df4104f
+size 228468
