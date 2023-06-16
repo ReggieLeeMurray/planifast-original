ba781c9af0b9f88af03a4c7457c7761b17259d07
Reggie Lee Murray <lee_murray@unitec.edu>
2023-06-16 10:45:36 -0600

    remove eventos appmodules/router/component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53347d0abc5f2adf58ed8c7496a18d9d069888ccbd1bea9ab883d6c9861d85ae
-size 6286
+oid sha256:371d15cc5771dfe86b0aa0cce2956c9222a2613e29448148ac17b819b2171e2b
+size 5663
